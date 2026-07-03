3207877afe38fece50cdcd04d7375973e8a0b23d
ibrahimAbelazimIbrahim <rfatk03@gmail.com>
2026-07-03 19:03:17 +0300

    اضافه داله جلب الرصيد وسجل العمليات

--- app.js
+++ app.js
@@ -7,27 +7,46 @@ const SUPERBASE_ANON_KEY = "sb_publishable_zP9Gfsg13JzG07TpVTsTkQ_17HUnRuk";
 
 const supabaseClient = supabase.createClient(SUPERBASE_URL, SUPERBASE_ANON_KEY);
 
-const balanceEL = document.getElementById("balance");
+const balanceEl = document.getElementById("balance");
 const amountInput = document.getElementById("amount-input");
 const btnIncome = document.getElementById("btn-income");
 const btnExpense = document.getElementById("btn-expense");
+const historyList = document.getElementById("history-list");
 
-// داله لجلب البيانات من قاعدة البيانات
-
+// داله جلب الرصيد وسجل العمليات
 async function loadData() {
-  try {
-    let { data: wallet, erro } = await supabaseClient
-      // جلب الرصيد
-      .from("wallet")
-      .select("balance")
-      .single();
+  //  جلب البيانات من قاعدة البيانات
+  let { data: wallet, erro1 } = await supabaseClient
+    // جلب الرصيد
+    .from("wallet")
+    .select("balance")
+    .single();
 
-    if (wallet) {
-      balanceEl.innerText = wallet.balance;
-    }
-  } catch (err) {
-    console.log("حصل خطا غير متوقع ");
+  if (wallet) {
+    balanceEl.innerText = wallet.balance;
+  }
+
+  //  جلب سجل العلميات
+  let { data: history, erro2 } = await supabaseClient
+    // جلب السجل
+    .from("history")
+    .select("*")
+    .order("id", { ascending: false });
+
+  //  <ul id="history-list"></ul>
+  //  historyList  as var
+  // li
+
+  if (history) {
+    historyList.innerHTML = ""; //clean
+    history.forEach((item) => {
+      const li = document.createElement("li");
+      li.innerText = `${item.type}: ${item.amount} ج.م`;
+      historyList.appendChild(li);
+    });
   }
 }
+
+
 
 loadData();
